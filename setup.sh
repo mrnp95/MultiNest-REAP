d22ae85f34d2f96bcbe834739ea18f6d3a6dc850
Reza <mrn31@cam.ac.uk>
2019-12-01 18:41:54 +0000

    setup.sh 2

--- setup.sh
+++ setup.sh
@@ -4,19 +4,22 @@ echo;
 echo "Installing MultiNest-REAP ...";
 echo;
 
-chmod a+x ./runMath
+chmod +x ./runMath
 cp -pR ./runMath /usr/local/bin/
 
+echo "Done!"
 echo;
 echo "Installing REAP ...";
 echo;
 
 bash ./reap_package/install.sh
 
+echo "Done!"
 echo;
 echo "Preparing examples ...";
 echo;
 
 cp -pR ./examples/HSMU_REAP.m ~/.Mathematica/Applications/
 
+echo "Done!"
 
